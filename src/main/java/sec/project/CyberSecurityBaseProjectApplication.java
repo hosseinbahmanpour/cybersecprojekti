c3627356fe2195cf6dab861a5a6796e1bf6859ec
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2017-01-27 08:39:00 +0200

    heikennetty tietoturvaa

--- src/main/java/sec/project/CyberSecurityBaseProjectApplication.java
+++ src/main/java/sec/project/CyberSecurityBaseProjectApplication.java
@@ -2,8 +2,10 @@ package sec.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class CyberSecurityBaseProjectApplication {
 
     public static void main(String[] args) throws Throwable {
