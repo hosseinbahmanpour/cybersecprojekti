bd16262a3b0329b487548e19dc8703db907373bb
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2017-01-27 16:31:00 +0200

    html formi hidden, mutta ei turvallisesti

--- src/main/java/sec/project/CyberSecurityBaseProjectApplication.java
+++ src/main/java/sec/project/CyberSecurityBaseProjectApplication.java
@@ -1,11 +1,13 @@
 package sec.project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
+@EnableAutoConfiguration
 public class CyberSecurityBaseProjectApplication {
 
     public static void main(String[] args) throws Throwable {
